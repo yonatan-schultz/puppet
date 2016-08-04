3257598754269bd9e3b002f5237cad94492f0677
Yonatan Schultz <yonatan.schultz@gmail.com>
2016-08-04 20:24:48 +0000

    adding home assistant and ldap servers

--- modules/ceph/manifests/init.pp
+++ modules/ceph/manifests/init.pp
@@ -8,7 +8,24 @@
 #
 class ceph {
 
-	$packages = ['ceph', 'ceph-common']
+	file { "/etc/apt/sources.list":
+		content => "deb http://mirrordirector.raspbian.org/raspbian/ testing main contrib non-free rpi",
+	}->
+
+	file { "/etc/apt/sources.list.d/ceph.list":
+		ensure	=> present,
+		contents => "deb http://ceph.com/debian-hammer/ jessie main"
+	}->
+
+	exec { "apt-get update":
+		command => "/usr/bin/apt-get update",
+	}->
+	
+	exec { "apt-get upgrade":
+		command => "/usr/bin apt-get upgrade -y",
+	}->
+
+	$packages = ['ceph', 'ceph-common', 'ceph-deploy']
 	package { $packages: ensure => 'installed' }
 
 }
