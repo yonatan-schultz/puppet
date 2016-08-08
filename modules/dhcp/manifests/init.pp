007d76c3e9294513bb7abe65a5f1bf38ce3303d0
Yonatan Schultz <yonatan.schultz@gmail.com>
2016-08-08 03:59:39 +0000

    adding isc-dhcp-server

--- modules/dhcp/manifests/init.pp
+++ modules/dhcp/manifests/init.pp
@@ -6,6 +6,13 @@ class dhcp {
 
 	package { 'isc-dhcp-server':
 		ensure => installed,
-	}
+	}->
 
+	file { '/etc/default/isc-dhcp-server':
+                ensure  => present,
+                source  => 'puppet:///modules/dhcp/isc-dhcp-server',
+                owner   => 'root',
+                group   => 'root',
+                mode    => 644,
+        }
 }
